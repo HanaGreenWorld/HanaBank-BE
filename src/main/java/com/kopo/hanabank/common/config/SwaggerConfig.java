3ec993f9b550b666de24d00d17d9c8058a66b90a
Jimin0304 <pjws0304@naver.com>
2025-10-15 19:50:36 +0900

    chore: config, type

--- src/main/java/com/kopo/hanabank/common/config/SwaggerConfig.java
+++ src/main/java/com/kopo/hanabank/common/config/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
                         .description("하나은행 서버 - 적금, 투자, 대출 관리 API")
                         .version("v1.0.0")
                         .contact(new Contact()
-                                .name("하나은행 개발팀")
-                                .email("hanabank@kopo.ac.kr")));
+                                .name("박지민")
+                                .email("jimin1299@naver.com")));
     }
 }
 
